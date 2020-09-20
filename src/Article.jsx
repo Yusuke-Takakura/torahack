a8782144eb280dadde00807f65fd906632fdf930
Yusuke-Takakura <p8g43blf@s.okayama-u.ac.jp>
2020-09-21 01:19:40 +0900

    第８回

--- src/Article.jsx
+++ src/Article.jsx
@@ -1,15 +1,17 @@
-import React from 'react';
-import LikeButton from './LikeButton'
+import React, { useState } from 'react';
+// import LikeButton from './LikeButton'
 
 //functional component
 const Article = (props) => {
+    const [isPublished, toggleIsPublished] = useState(false);
+
     return (
         <div>
             <h2>{props.title}</h2>
             <p>{props.order}</p>
             <label htmlFor={"check"}>公開状態:</label>
-            <input type="checkBox" checked={props.isPublished} id="check" onClick={() => props.toggle()} />
-            <LikeButton count={props.count} />
+            <input type="checkBox" checked={isPublished} id="check" onClick={() => toggleIsPublished(!isPublished)} />
+            {/* <LikeButton count={props.count} /> */}
         </div>
     )
 };
--- src/Blog.jsx
+++ src/Blog.jsx
@@ -7,73 +7,48 @@ import * as FooBar from './components/FooBar';
 import Hoge from './components/Hoge';
 
 //クラスコンポーネント　reactのcomponentを継承している
-class Blog extends React.Component {
-    //classなのでconstructorの初期化が必要
-    //constractorはstateの初期化
-    constructor(props) {
-        //superを指定するとthis.propsが使用可能になる
-        super(props);
-        this.state = {
-            //Articleを非公開
-            isPublished: false,
-            count: 0
-        };
-    };
+const Blog = () => {
+    // //最初のrenderが走ったときに一度だけ呼ばれる
+    // //リスナーの設定やAPI通信に使われる
+    // componentDidMount() {
+    //     //ボタンがクリックされたらいいねをカウントアップする
+    //     //関数は関数型か関数だけを渡す記述　今回は関数だけを渡している
+    //     document.getElementById("counter").addEventListener('click', this.countUp)
+    // }
 
-    //最初のrenderが走ったときに一度だけ呼ばれる
-    //リスナーの設定やAPI通信に使われる
-    componentDidMount() {
-        //ボタンがクリックされたらいいねをカウントアップする
-        //関数は関数型か関数だけを渡す記述　今回は関数だけを渡している
-        document.getElementById("counter").addEventListener('click', this.countUp)
-    }
+    // //renderが走るたびに呼ばれる
+    // //スクロールイベントや条件付きイベント
+    // componentDidUpdate() {
+    //     //いいね数か10を超えたらカウントをリセットする
+    //     if (this.state.count >= 10) {
+    //         this.setState({ count: 0 })
+    //     }
+    // }
 
-    //renderが走るたびに呼ばれる
-    //スクロールイベントや条件付きイベント
-    componentDidUpdate() {
-        //いいね数か10を超えたらカウントをリセットする
-        if (this.state.count >= 10) {
-            this.setState({ count: 0 })
-        }
-    }
-
-    //コンポーネントが破棄される直前
-    //リソースを解除するため
-    //リスナーの解除など
-    componentWillUnmount() {
-        document.getElementById("counter").removeEventListener("click", this.countUp)
-    }
+    // //コンポーネントが破棄される直前
+    // //リソースを解除するため
+    // //リスナーの解除など
+    // componentWillUnmount() {
+    //     document.getElementById("counter").removeEventListener("click", this.countUp)
+    // }
 
     //公開状態を反転させる
-    togglePublished = () => {
-        this.setState({
-            isPublished: !this.state.isPublished
-        });
-    };
 
-    countUp = () => {
-        this.setState({
-            count: this.state.count + 1
-        })
-    };
+    // countUp = () => {
+    //     this.setState({
+    //         count: this.state.count + 1
+    //     })
+    // };
 
-    //VDOMを描画
-    render() {
-        const authorName = "ultraK"
-        return (
-            <>
-                <Article
-                    title={"Reactの使い方"}
-                    isPublished={this.state.isPublished}
-                    toggle={() => this.togglePublished()}
-                    count={this.state.count}
-                />
-                <FooBar.Foo />
-                <FooBar.Bar />
-                <Hoge />
-            </>
-        )
-    }
+    return (
+        <>
+            <Article
+                title={"Reactの使い方"}
+            />
+        </>
+    )
+
+
 }
 //titleがprops,{}の中がjavascript
 
